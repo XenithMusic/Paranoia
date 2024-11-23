b3cfa53adf3ca3f24c207810e785599453d18e23
XenithMusic <36333004+XenithMusic@users.noreply.github.com>
2024-11-23 12:00:23 -0500

    Remove unnecessary extern, add pragma. + pragma once - extern "C"

--- src/types.h
+++ src/types.h
@@ -1,30 +1,29 @@
+#include "stdint.h"
+
 #pragma once
 
-extern "C" {
+enum MMAPAllocationType : uint32_t {
+	ALLOCATED = 0x00,
+	AVAILABLE = 0x01,
+	RESERVED = 0x02
+};
 
-    enum MMAPAllocationType : uint32_t {
-    	ALLOCATED = 0x00,
-    	AVAILABLE = 0x01,
-    	RESERVED = 0x02
-    };
+typedef struct {
+    uint32_t flags;
+    uint32_t mem_lower;
+    uint32_t mem_upper;
+    uint32_t mmap_length;
+    uint32_t mmap_addr;
+} multiboot_info;
 
-	typedef struct {
-        uint32_t flags;
-        uint32_t mem_lower;
-        uint32_t mem_upper;
-        uint32_t mmap_length;
-        uint32_t mmap_addr;
-    } multiboot_info;
+typedef struct {
+    uint32_t size; // size of the `mmap` entry
+    uint32_t addr; // start of the memory region
+    uint32_t len;  // length of the entry (`size` is for the entry)
+    enum MMAPAllocationType type; // type of memory
+} mmap_entry;
 
-    typedef struct {
-        uint32_t size; // size of the `mmap` entry
-        uint32_t addr; // start of the memory region
-        uint32_t len;  // length of the entry (`size` is for the entry)
-        enum MMAPAllocationType type; // type of memory
-    } mmap_entry;
-
-    enum MMAPAccessType {
-    	ALLOCATE, // Find a location to allocate memory (target = how much)
-    	FREE      // Find a location to free memory     (target = location)
-    };
-}
+enum MMAPAccessType {
+	ALLOCATE, // Find a location to allocate memory (target = how much)
+	FREE      // Find a location to free memory     (target = location)
+};

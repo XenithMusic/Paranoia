d918d7f5f8632cb05d6d272cd208eb2dc76c03f2
XenithMusic <36333004+XenithMusic@users.noreply.github.com>
2024-11-23 20:44:10 -0500

    Working (but very basic) memory allocator

--- src/memory.h
+++ src/memory.h
@@ -1,6 +1,29 @@
+#include <stddef.h>
 #include "types.h"
+#include "terminal.h"
+#include "string.h"
+#include "const.h"
+#include "fail.h"
+
+#define MEMORY_POOL_SIZE 512*MEBIBYTES // 512 MiB
+#define BLOCK_SIZE 512 // 512B
+#define NUM_BLOCKS MEMORY_POOL_SIZE/BLOCK_SIZE // 512 MiB
+
+#define MEM_START (0x1*MEBIBYTES)
+
+#if (NUM_BLOCKS%1) != 0
+#error "(-2032) NUM_BLOCKS should be an integer."
+#endif
+
+#if CONST_KERNELSIZE >= NUM_BLOCKS
+#error "(-2033) CONST_KERNELSIZE cannot exceed the available memory pool."
+#endif
 
 namespace Allocator {
 
-	void init(multiboot_info* mbi);
+    void init();
+
+    void *malloc(size_t size);
+
+    void free(void* pointer);
 }
